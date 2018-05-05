f7b6b538d6e17a8ffaea23703d51b9d2d8d1353b
Emelyne Riechter <37333559+EmyRiechter@users.noreply.github.com>
2018-05-05 12:22:57 -0300

    Add files via upload

---
+++ ParOuImpar/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ParOuImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+
+                Console.WriteLine("Digite um numero: ");
+                long n = long.Parse(Console.ReadLine());
+
+                if (n % 2 == 0)
+                {
+                    Console.WriteLine("O número " + n + " é par" + Environment.NewLine);
+
+                }
+                else
+                {
+
+                    Console.WriteLine("O número " + n + " é impar" + Environment.NewLine);
+                }
+
+            }
+
+        }
+
+    }
+    
+}
+
+
+
