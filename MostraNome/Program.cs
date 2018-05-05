7cb4bc901b0e6d2f36acb787c85027cda036efd2
Emelyne Riechter <37333559+EmyRiechter@users.noreply.github.com>
2018-05-05 12:29:47 -0300

    Add files via upload

---
+++ MostraNome/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MostraNome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                string nome;
+                Console.WriteLine("Qual seu nome? ");
+                nome = Console.ReadLine();
+                Console.WriteLine("Olá " + nome + Environment.NewLine);
+
+            } while (true);
+
+        }
+    }
+}
