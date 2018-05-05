a618e88b77891e1e787051794cb81f4384245c42
Emelyne Riechter <37333559+EmyRiechter@users.noreply.github.com>
2018-05-05 12:33:36 -0300

    Add files via upload

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            for (n = 1; n <= 100; n++)
+
+                if (n % 3 == 0 && n % 5 == 0)
+                {
+                    Console.WriteLine("FizBuzz");
+                }
+                else if (n % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (n % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(n);
+                }
+                
+        }
+    }
+}
